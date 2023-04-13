0bcfe5a53509d92c4d0593187bf6470e5a22737f
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-13 19:53:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
-from scipy.stats import ks_2samp
+from scipy.stats import anderson_ksamp
 import pandas as pd
 import numpy as np
 
 chat_id = 704471350
 
 def solution(x: np.array, y: np.array) -> bool:
-    KStest = ks_2samp(x, y)
+    Anderson_test = anderson_ksamp([x,y])
     alpha = 0.1
-    if alpha < KStest.pvalue:
+    if alpha < Anderson_test.pvalue:
       res = False
     else:
       res = True
