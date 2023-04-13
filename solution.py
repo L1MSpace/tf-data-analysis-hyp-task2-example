50851a39bd825b95fc2085fe3b414e55cc07d222
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-13 10:46:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,36 +1,16 @@
+import scipy.stats as stats
 import pandas as pd
 import numpy as np
-from math import sqrt
 
 chat_id = 704471350
 
 def solution(x: np.array, y: np.array) -> bool:
-    m = len(x)
-    n = len(y)
-    x_avr = sum(x)/m
-    y_avr = sum(y)/n
-
-    s_t1 = 0
-    S_x = 0
-    for i in range(m):
-      s_t1 = (x[i] - x_avr)**2
-      S_x +=s_t1
-
-    s_t2 = 0
-    S_y = 0
-    for i in range(n):
-      s_t2 = (y[i] - y_avr)**2
-      S_y +=s_t2
-
-    s2_x = S_x/(m-1)
-    s2_y = S_y/(n-1)
-
-    t = ((x_avr-y_avr)/(sqrt((m-1)*s2_x + (n-1)*s2_y))) * sqrt( (m*n*(m+n-2))/(m+n) )
-
-    t_crit = 2.33
-    if t < t_crit:
+    Ttest = stats.ttest_ind(a=x, b=y, equal_var=False)
+    alpha = 0.1
+    print(Ttest.pvalue)
+    print(alpha)
+    if alpha < Ttest.pvalue:
       res = False
     else:
       res = True
-
     return res
