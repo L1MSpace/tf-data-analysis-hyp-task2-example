8d0bd739efd4d24483b0eabf698e018a50f65bf3
L1MSpace <129736244+L1MSpace@users.noreply.github.com>
2023-04-13 15:48:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,13 @@
-import scipy.stats as stats
+from scipy.stats import ks_2samp
 import pandas as pd
 import numpy as np
 
 chat_id = 704471350
 
 def solution(x: np.array, y: np.array) -> bool:
-    Ttest = stats.ttest_ind(a=x, b=y, equal_var=False)
+    KStest = ks_2samp(x, y)
     alpha = 0.1
-    print(Ttest.pvalue)
-    print(alpha)
-    if alpha < Ttest.pvalue:
+    if alpha < KStest.pvalue:
       res = False
     else:
       res = True
